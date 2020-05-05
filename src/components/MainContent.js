096150addcc9a04ba19615a147640555f4c6c298
rs.rupesh95 <rs.rupesh95@gmail.com>
2020-05-05 23:07:19 +0530

    final touch

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,26 +1,35 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import Settings from "./Settings"
-import Notification from "./Notification"
-import Mytask from "./Mytask"
+import Settings from "./Settings";
+import Notification from "./Notification";
+import Mytask from "./Mytask";
+import Typography from "@material-ui/core/Typography";
 
+const Home = () => {
+  return (
+    <>
+      <Typography variant="h4">
+        Use the Side Navigations to go to a page
+      </Typography>
+    </>
+  );
+};
 
 const MainContent = () => {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component = {Settings}>
-        </Route>
-        <Route exact path="/profile/settings" component = {Settings}>
-        </Route>
-        <Route exact path="/profile/notification" component = {Notification}>
-        </Route>
-        <Route exact path="/mytask/:id" component={Mytask}>
-        </Route>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/profile/settings" component={Settings}></Route>
+        <Route
+          exact
+          path="/profile/notification"
+          component={Notification}
+        ></Route>
+        <Route exact path="/mytask/:id" component={Mytask}></Route>
       </Switch>
     </div>
   );
 };
-
 
 export default MainContent;
